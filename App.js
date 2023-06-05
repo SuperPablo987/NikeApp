4e50812058a84a69dc7f6be040e048ad639e616e
Pablo <63468752+SuperPablo987@users.noreply.github.com>
2023-06-05 16:23:02 -0600

    First Image now attempting to get a flatlist

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import products from './src/data/products';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => (
+          <Image 
+            source={{
+              uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png",
+            }}
+            style={styles.image}
+          />
+        )}
+      />
+      
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +29,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {width: "100%", 
+  aspectRatio: 1,
   },
 });
