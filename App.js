bae0fdf81a887fce525c9228fc38b21e02d53ba5
Pablo <63468752+SuperPablo987@users.noreply.github.com>
2023-06-06 09:41:26 -0600

    Completed Products Screen

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
-import products from './src/data/products';
+import { StyleSheet, View} from 'react-native';
+import ProductsScreen from './src/screens/ProductsScreens';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FlatList
-        data={products}
-        renderItem={({ item }) => (
-          <View style={styles.itemContainer}>
-            <Image source={{ uri: item.image}} style={styles.image} />
-          </View>
-        )}
-      />
+      <ProductsScreen/>
 
       <StatusBar style="auto" />
     </View>
@@ -25,12 +18,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  itemContainer: {
-    width: "50%",
-    padding: 1,
-  },
-  image: {width: "100%", 
-  aspectRatio: 1,
   },
 });
