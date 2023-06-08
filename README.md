6d493591f98cf495c73f6c88c39558cb5f245004
Pablo <63468752+SuperPablo987@users.noreply.github.com>
2023-06-08 10:56:20 -0600

    Started Navigation

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 React Native App with Redux And later Stripe
 
 https://www.youtube.com/watch?v=Gvxv6JCabA8
-41:38
+1:48:26
---
+++ src/components/navigation.js
@@ -0,0 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import ProductsScreen from "../screens/ProductsScreens";
+import ProductDetailsScreen from "../screens/ProductDetailsScreen";
+import ShoppingCart from "../screens/ShoppingCart";
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Products" component={ProductsScreen} />
+            </Stack.Navigator>
+
+        </NavigationContainer>
+    );
+};
+
+export default Navigation;
