c691ffce1bb4fe51925891b2de75a2ea961918b9
Pablo <63468752+SuperPablo987@users.noreply.github.com>
2023-06-08 09:58:31 -0600

    Completed Shopping Cart Screen

--- src/screens/ShoppingCart.js
+++ src/screens/ShoppingCart.js
@@ -1,29 +1,36 @@
-import { Text, View, FlatList, StyleSheet } from 'react-native';
+import { Text, View, FlatList, StyleSheet, Pressable } from 'react-native';
 import cart from '../data/cart';
 import CartListItem from '../components/CartListItem';
 
+const ShoppingCartTotals = () => (
+    <View style={styles.totalsContainer}>
+        <View style={styles.row}>
+            <Text style={styles.text}>Subtotal</Text>
+            <Text style={styles.text}>410,00 US$</Text>
+        </View>
+        <View style={styles.row}>
+            <Text style={styles.text}>Delivery</Text>
+            <Text style={styles.text}>10,00 US$</Text>
+        </View>
+        <View style={styles.row}>
+            <Text style={styles.textBold}>Total</Text>
+            <Text style={styles.textBold}>420,00 US$</Text>
+        </View>
+    </View>
+)
+
 const ShoppingCart = () => {
     return (
+        <>
         <FlatList 
             data={cart} 
             renderItem={({ item }) => <CartListItem cartItem={item} />}
-            ListFooterComponent={() => (
-                <View style={styles.totalsContainer}>
-                    <View style={styles.row}>
-                        <Text style={styles.text}>Subtotal</Text>
-                        <Text style={styles.text}>410,00 US$</Text>
-                    </View>
-                    <View style={styles.row}>
-                        <Text style={styles.text}>Delivery</Text>
-                        <Text style={styles.text}>10,00 US$</Text>
-                    </View>
-                    <View style={styles.row}>
-                        <Text style={styles.textBold}>Total</Text>
-                        <Text style={styles.textBold}>420,00 US$</Text>
-                    </View>
-                </View>
-            )}
+            ListFooterComponent={ShoppingCartTotals}
         />
+        <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Checkout</Text>
+        </Pressable>
+        </>
     );
 };
 
@@ -47,6 +54,21 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontWeight: '500',
     },
+    button:{
+        position: 'absolute',
+        backgroundColor: 'black',
+        bottom: 30,
+        width: '90%',
+        alignSelf: 'center',
+        padding: 20,
+        borderRadius: 100,
+        alignItems: "center",
+      },
+      buttonText:{
+        color: 'white',
+        fontWeight: '500',
+        fontSize: 16,
+      },
 });
 
 export default ShoppingCart;
